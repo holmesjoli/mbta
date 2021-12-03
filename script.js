1547355daf62148640a0e56aa037f8023471e1c2
Joli Holmes <holmesjoli@gmail.com>
2021-12-02 19:31:03 -0500

    loaded multiple data sources as json files

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-d3.json("./data/processed/geo_stations.json").then(function(data) {
-    console.log(data);
+let pth = "./data/processed/";
 
+// d3.json(path.join(pth, "geo_station_connections.json")).then(function(edges) {
+//     d3.json(path.join(pth, "geo_stations.json")).then(function(nodes) {
+d3.json(pth +"geo_station_connections.json").then(function(geo_edges) {
+    d3.json(pth +"geo_stations.json").then(function(geo_nodes) {
 
+        console.log("geo_edges", geo_edges);
+        console.log("geo_nodes", geo_nodes);
+    });
 });
