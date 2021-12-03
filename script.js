9990ee68f7768655b739fc2d1712307266150630
Joli Holmes <holmesjoli@gmail.com>
2021-12-03 13:25:35 -0500

    changed the order of the points and lines

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+//Title Unique Array
+//Description the unique value of a variable from the data
+//Return array
+function unique_array(data, variable) {
+
+    const u = [];
+
+    data.forEach(function(d) {
+        if (u.indexOf(d[variable]) === -1) {
+            u.push(d[variable]);
+        }
+    });
+
+    return u;
+};
+
 let pth = "./data/processed/";
 
 // d3.json(path.join(pth, "geo_station_connections.json")).then(function(edges) {
@@ -24,7 +40,9 @@ d3.json(pth + "beck_station_connections.json").then(function(beck_links) {
             min: d3.min(beck_nodes, function(d) {return d.y;})
         };
 
-        console.log(y);
+        const lines = unique_array(beck_links, "line");
+
+        console.log(lines);
 
         let svg = d3.select("#chart")
                     .append("svg")
@@ -39,6 +57,22 @@ d3.json(pth + "beck_station_connections.json").then(function(beck_links) {
                     .domain([x.min, x.max])
                     .range([margin.left, width - margin.right]);
 
+        let colorScale = d3.scaleOrdinal()
+        .domain(lines)
+        .range(["#018447", "#018447", "#018447", "#018447", "#018447", "#E12D27", 
+                "#E87200", "#2F5DA6"]);
+
+        svg.append("g").selectAll("line")
+                .data(beck_links)
+                .enter()
+                .append("line")
+                .attr("x1", function(d) {return xScale(d.x1);})
+                .attr("x2", function(d) {return xScale(d.x2);})
+                .attr("y1", function(d) {return yScale(d.y1);})
+                .attr("y2", function(d) {return yScale(d.y2);})
+                .style("stroke", function(d) {return colorScale(d.line);})
+                .style("stroke-width", 3);
+
         svg.append("g").selectAll('circle')
                     .data(beck_nodes)
                     .enter()
@@ -47,16 +81,39 @@ d3.json(pth + "beck_station_connections.json").then(function(beck_links) {
                     .attr("cy", function(d) {return yScale(d.y);})
                     .attr("r", 5);
 
-        svg.append("g").selectAll("line")
-            .data(beck_links)
-            .enter()
-            .append("line")
-            .attr("x1", function(d) {return xScale(d.x1);})
-            .attr("x2", function(d) {return xScale(d.x2);})
-            .attr("y1", function(d) {return yScale(d.y1);})
-            .attr("y2", function(d) {return yScale(d.y2);})
-            .style("stroke", "#018447")
-            .style("stroke-width", 3);
+
+        let tooltip = d3.select("#chart")
+            .append("div")
+            .attr("class", "tooltip");
+          
+          //only selecting circles which have already be drawn for the first data join
+          function tt() {
+            svg.selectAll("circle").on("mouseover", function(e, d) {
+      
+            let cx = +d3.select(this).attr("cx")+20;
+            let cy = +d3.select(this).attr("cy")-10;
+      
+            tooltip.style("visibility","visible") 
+                .style("left", `${cx}px`)
+                .style("top", `${cy}px`)
+                .html(`<b>Id:</b> ${d.id}<br><b>Name:</b> $${d.name}`);
+      
+            d3.select(this)
+                .attr("stroke","#F6C900")
+                .attr("stroke-width",2);
+      
+            }).on("mouseout", function() {
+      
+            tooltip.style("visibility","hidden");
+      
+            d3.select(this)
+                .attr("stroke","none")
+                .attr("stroke-width",0);
+                  
+            });
+          };
+
+          tt();
 
     });
 });
