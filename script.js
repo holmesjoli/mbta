4192b8deb4ccea8810e4da9f2492d93e14eccfc5
Joli Holmes <holmesjoli@gmail.com>
2021-12-06 21:46:30 -0500

    updated variable names

--- script.js
+++ script.js
@@ -29,15 +29,15 @@ d3.json(pth + "beck_station_connections.json").then(function(beck_links) {
         const margin = {top: 100, left: 100, right: 200, bottom: 125};
 
         const x = {
-            max: d3.max(beck_nodes, function(d) {return d.x;}),
-            min: d3.min(beck_nodes, function(d) {return d.x;})
+            max: d3.max(beck_nodes, function(d) {return d.beck_x;}),
+            min: d3.min(beck_nodes, function(d) {return d.beck_x;})
         };
 
         console.log(x);
 
         const y = {
-            max: d3.max(beck_nodes, function(d) {return d.y;}),
-            min: d3.min(beck_nodes, function(d) {return d.y;})
+            max: d3.max(beck_nodes, function(d) {return d.beck_y;}),
+            min: d3.min(beck_nodes, function(d) {return d.beck_y;})
         };
 
         const lines = unique_array(beck_links, "line");
@@ -66,10 +66,10 @@ d3.json(pth + "beck_station_connections.json").then(function(beck_links) {
                 .data(beck_links)
                 .enter()
                 .append("line")
-                .attr("x1", function(d) {return xScale(d.x1);})
-                .attr("x2", function(d) {return xScale(d.x2);})
-                .attr("y1", function(d) {return yScale(d.y1);})
-                .attr("y2", function(d) {return yScale(d.y2);})
+                .attr("x1", function(d) {return xScale(d.beck_x1);})
+                .attr("x2", function(d) {return xScale(d.beck_x2);})
+                .attr("y1", function(d) {return yScale(d.beck_y1);})
+                .attr("y2", function(d) {return yScale(d.beck_y2);})
                 .style("stroke", function(d) {return colorScale(d.line);})
                 .style("stroke-width", 3);
 
@@ -77,43 +77,43 @@ d3.json(pth + "beck_station_connections.json").then(function(beck_links) {
                     .data(beck_nodes)
                     .enter()
                     .append("circle")
-                    .attr("cx", function(d) {return xScale(d.x);})
-                    .attr("cy", function(d) {return yScale(d.y);})
+                    .attr("cx", function(d) {return xScale(d.beck_x);})
+                    .attr("cy", function(d) {return yScale(d.beck_y);})
                     .attr("r", 5);
 
-
+        console.log(beck_nodes);
         let tooltip = d3.select("#chart")
             .append("div")
             .attr("class", "tooltip");
-          
-          //only selecting circles which have already be drawn for the first data join
-          function tt() {
-            svg.selectAll("circle").on("mouseover", function(e, d) {
-      
-            let cx = +d3.select(this).attr("cx")+20;
-            let cy = +d3.select(this).attr("cy")-10;
-      
-            tooltip.style("visibility","visible") 
-                .style("left", `${cx}px`)
-                .style("top", `${cy}px`)
-                .html(`<b>Id:</b> ${d.id}<br><b>Name:</b> $${d.name}`);
-      
-            d3.select(this)
-                .attr("stroke","#F6C900")
-                .attr("stroke-width",2);
-      
-            }).on("mouseout", function() {
-      
-            tooltip.style("visibility","hidden");
-      
-            d3.select(this)
-                .attr("stroke","none")
-                .attr("stroke-width",0);
-                  
-            });
-          };
+        
+        //only selecting circles which have already be drawn for the first data join
+        function tt() {
+        svg.selectAll("circle").on("mouseover", function(e, d) {
+    
+        let cx = +d3.select(this).attr("cx")+20;
+        let cy = +d3.select(this).attr("cy")-10;
+    
+        tooltip.style("visibility","visible") 
+            .style("left", `${cx}px`)
+            .style("top", `${cy}px`)
+            .html(`<b>Id:</b> ${d.name}`);
+    
+        d3.select(this)
+            .attr("stroke","#F6C900")
+            .attr("stroke-width",2);
+    
+        }).on("mouseout", function() {
+    
+        tooltip.style("visibility","hidden");
+    
+        d3.select(this)
+            .attr("stroke","none")
+            .attr("stroke-width",0);
+                
+        });
+        };
 
-          tt();
+        tt();
 
     });
 });
