7cbe8cd11969551e4e9f26775c8514fde3998eef
Joli Holmes <holmesjoli@gmail.com>
2021-12-07 21:03:10 -0500

    updated to include more data

--- js/carousel.js
+++ js/carousel.js
@@ -1,30 +1,36 @@
 const img = document.getElementById('carousel');
+const title = document.getElementById('title');
+const source = document.getElementById('source');
 const rightBtn = document.getElementById('right-btn');
 const leftBtn = document.getElementById('left-btn');
 
-let pictures = ['./images/pre_beck_1933.jpg', './images/beck_1933.jpg', 'images/boston_1905.jpg', 'images/boston_recent.jpg'];
+let pictures = [{src: './images/pre_beck_1933.jpg',
+                title: "London Underground Railroad design"},
+                {src: './images/beck_1933.jpg', title: "Beck's London Underground Railroad redesign"},
+                {src: 'images/boston_1905.jpg', title: "Boston railroad expansion plan 1905"},
+                {src: 'images/boston_recent.jpg', title: "Boston T-Map"}]
 
-img.src = pictures[0];
-console.log(img.src);
+img.src = pictures[0].src;
+
 let position = 0;
 
 const moveRight = () => {
     if (position >= pictures.length - 1) {
         position = 0
-        img.src = pictures[position];
+        img.src = pictures[position].src;
         return;
     }
-    img.src = pictures[position + 1];
+    img.src = pictures[position + 1].src;
     position++;
 }
 
 const moveLeft = () => {
     if (position < 1) {
         position = pictures.length - 1;
-        img.src = pictures[position];
+        img.src = pictures[position].src;
         return;
     }
-    img.src = pictures[position - 1];
+    img.src = pictures[position - 1].src;
     position--;
 }
 
