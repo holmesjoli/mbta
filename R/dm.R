7f269d966c6587f25e6b71f59fc11a6e70766a81
Joli Holmes <holmesjoli@gmail.com>
2021-12-12 10:59:07 -0500

    grouped order by line group

--- R/dm.R
+++ R/dm.R
@@ -64,6 +64,50 @@ dm_links <- function(network) {
   return(network)
 }
 
+dm_lat_long <- function(network, green = FALSE) {
+  
+  df <- network$lat_long %>% do.call(rbind, .) %>%
+    as.data.frame() %>%
+    dplyr::rename(x = V1,
+                  y = V2) %>%
+    dplyr::mutate(name = names(network$lat_long))
+  
+  if (green) {
+    
+    df <- df %>%
+      dplyr::mutate(x = x - 12.24264,
+                    y = y + 1.050253
+      )
+    
+  } else {
+    
+    orange <- paste("place",
+                    c("forhl", "grnst", "sbmnl" ,"jaksn", "rcmnl", "rugg", "masta", 
+                      "bbsta", "tumnl", "chncl", "dwnxg", "state", "ccmnl", "sull",
+                      "welln", "mlmnl", "ogmnl"), sep="-")
+    
+    blue <- paste("place",
+                  c("wondl", "rbmnl", "bmmnl", "sdmnl", "orhte", "wimnl", "aport",
+                    "mvbcl", "aqucl", "state", "gover", "bomnl"), sep="-")
+    
+    ord <- c(paste0("orange-", orange), paste0("blue-", blue))
+    
+    df <- df %>%
+      dplyr::mutate(orange = name %in% orange,
+                    blue = name %in% blue) %>%
+      tidyr::pivot_longer(names_to = "group", values_to = "values", -c(x, y, name)) %>%
+      dplyr::filter(values) %>%
+      dplyr::select(-values) %>%
+      dplyr::mutate(order = paste(group, name, sep = "-"))
+    
+    df <- df[match(ord, df$order),]
+
+  }
+  
+  return(df)
+}
+
+
 pth <- "./data/processed/"
 
 green_line <- "https://raw.githubusercontent.com/mbtaviz/green-line-release/gh-pages/data/station-network.json"
@@ -225,6 +269,7 @@ lines <- sf::read_sf("./data/raw/MBTA_ARC.shp") %>%
 lines <- lines %>%
   sf::st_coordinates() %>%
   as.data.frame() %>%
+  dplyr::group_by(L2) %>%
   dplyr::mutate(order = seq(1, dplyr::n(), 1)) %>%
   dplyr::inner_join(lines %>% dplyr::mutate(L2 = seq(1, dplyr::n(), 1))) %>%
   dplyr::rename(x = X, y = Y, group = L2) %>% 
