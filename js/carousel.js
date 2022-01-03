529fc47666a8c72ed33f72bf201fd2c91d09b179
Joli Holmes <holmesjoli@gmail.com>
2022-01-03 16:27:37 -0500

    updates

--- js/carousel.js
+++ js/carousel.js
@@ -11,26 +11,26 @@ const past = [{src: "images/boston_1967.jpg",
                 title: "Boston MBTA 1967",
                 href: "https://www.flickr.com/photos/vanshnookenraggen/3186247755/in/album-72157594460286528/"}];
 
-const present = [{
-                "src": 'images/boston_recent.jpg', 
-                "title": "Boston MBTA 2013", 
-                "href": "https://mapa-metro.com/mapas/Boston/mapa-metro-boston.jpg"}];
+const present = [{src: "images/boston_recent.jpg", 
+                title: "Boston MBTA 2013", 
+                href: "https://mapa-metro.com/mapas/Boston/mapa-metro-boston.jpg"}];
 
 img.src = past[0].src;
 title.textContent = past[0].title;
 source.href = past[0].href;
 
 const showPast = () => {
-
     img.src = past[0].src;
     title.textContent = past[0].title;
     source.href = past[0].href;
+    return;
 };
 
 const showPresent = () => {
     img.src = present[0].src;
     title.textContent = present[0].title;
     source.href = present[0].href;
+    return;
 };
 
 presentBtn.addEventListener("click", showPresent);
